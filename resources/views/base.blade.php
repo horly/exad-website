f20e8178ef8d9c9f57ea94e5eb32906abae5ed72
horly <andmatboy@gmail.com>
2024-11-15 11:21:19 +0100

    Email set

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="icon" rel="shortcut icon" href="{{ asset('assets/logo/exad-1200x1200.jpg') }}" type="image/jpg">
+
         <title>{{ config('app.name') }} - @yield('title')</title>
 
         <!-- Fonts -->
